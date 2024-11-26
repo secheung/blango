8c802a3e0ff9e10d4f53edfac147ab7062ee07a8
Samuel Cheung <samuel.e.cheung@gmail.com>
2024-11-26 17:58:15 +0000

    Finish database optimization

--- blog/views.py
+++ blog/views.py
@@ -7,9 +7,16 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+def get_ip(request):
+    from django.http import HttpResponse
+    return HttpResponse(request.META['REMOTE_ADDR'])
+
 # Create your views here.
 def index(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now())
+    posts = (
+        Post.objects.filter(published_at__lte=timezone.now())
+        .select_related("author")
+    )
     logger.debug("Got %d posts", len(posts))
 
     return render(request, "blog/index.html", {"posts":posts})
